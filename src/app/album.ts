eb831fba2659562ab790a28b99b2a4d38d74c988
SuHamza <suzan.hamza@gmail.com>
2019-10-29 21:20:47 +0200

    Using Interfaces to Describe Data

--- src/app/album.ts
+++ src/app/album.ts
@@ -2,6 +2,8 @@ import { Track } from './track';
 
 export interface Album {
 
-    name, releaseDate, coverImage: string;
+    name: string;
+    releaseDate: string;
+    coverImage: string;
     tracks: Track[];
 }
